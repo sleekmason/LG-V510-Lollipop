769e0b7e914ed1150f07d2753133c82f105c22bd
sleekmason <sleekmason@gmail.com>
2014-12-20 12:46:02 -0600

    Added_sed_options_to_remove_default_governer_from_ramdisk

--- KernelMaker/kernel/edit_ramdisk.sh
+++ KernelMaker/kernel/edit_ramdisk.sh
@@ -8,16 +8,13 @@ gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
 cd /
 rm /tmp/ramdisk/boot.img-ramdisk.gz
 rm /tmp/boot.img-ramdisk.gz
-cd /tmp/ramdisk/
+cd /tmp/ramdisk/ 
+ #remove governor overrides, use kernel default
+sed -i '/\/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc
+sed -i '/\/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc
+sed -i '/\/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc
+sed -i '/\/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc  
 sed -i "/\b\(setprop ro.tether.denied true\)\b/d" init.palman.rc
-sed -i '/write \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governer "ondemand"/c \   write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governer "wheatley"'
-init.palman.rc
-sed -i '/write \/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governer "ondemand"/c \   write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governer "wheatley"'
-init.palman.rc
-sed -i '/write \/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_governer "ondemand"/c \   write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governer "wheatley"'
-init.palman.rc
-sed -i '/write \/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_governer "ondemand"/c \   write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governer "wheatley"'
-init.palman.rc    
 sed -i '/write \/proc\/sys\/vm\/dirty_expire_centisecs 200/c \   write /proc/sys/vm/dirty_expire_centisecs 300' init.rc
 sed -i '/write \/proc\/sys\/vm\/dirty_background_ratio  5/c \    write /proc/sys/vm/dirty_background_ratio  10' init.rc
 find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
