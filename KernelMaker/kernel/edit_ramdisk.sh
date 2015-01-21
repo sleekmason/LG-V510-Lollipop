8c426c64108c72eab584e49c26ce36fa957b1b21
sleekmason <sleekmason@gmail.com>
2015-01-20 20:32:54 -0600

    Enabled exfat support

--- KernelMaker/kernel/edit_ramdisk.sh
+++ KernelMaker/kernel/edit_ramdisk.sh
@@ -15,6 +15,7 @@ sed -i '/\/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governor/d' /tmp/ra
 sed -i '/\/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc
 sed -i '/\/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.palman.rc  
 sed -i "/\b\(setprop ro.tether.denied true\)\b/d" init.palman.rc
+sed -i '/insmod \/system\/lib\/modules\/texfat.ko/c \   insmod /system/lib/modules/exfat.ko' init.palman.rc
 sed -i '/write \/proc\/sys\/vm\/dirty_expire_centisecs 200/c \   write /proc/sys/vm/dirty_expire_centisecs 300' init.rc
 sed -i '/write \/proc\/sys\/vm\/dirty_background_ratio  5/c \    write /proc/sys/vm/dirty_background_ratio  10' init.rc
 find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
